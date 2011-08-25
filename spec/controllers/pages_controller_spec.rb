2722baac5628216034a9cd9b7b9fdfecbaf7ed35
yang0230 <bj_liyang@yahoo.com>
2011-08-25 12:14:26 -0700

    Down with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -15,5 +16,11 @@ describe PagesController do
       response.should be_success
     end
   end
-
+  
+  describe "GET 'about'" do
+      it "should be successful" do
+        get 'about'
+        response.should be_success
+      end
+    end
 end
